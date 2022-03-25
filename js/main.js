1b20ab4d30277da32119b19835debd0581103210
FedericoCapitani <federico.capitani@outlook.it>
2022-03-25 10:42:44 +0100

    aggiungere controlli

--- js/main.js
+++ js/main.js
@@ -2,14 +2,20 @@
 
 // chiedere all'utente quanti chilometri è lunga la sua tratta
 const lunghezza_tratta = parseInt(prompt('Inserisci quanti Km devi percorrere'));
+if (isNaN(lunghezza_tratta)){
+    alert('devi inserire un numero per la distanza da percorrere')
+}
 
 // chiedere all'utente l'età
 const età_utente = parseInt(prompt('Inserisci la tua età'));
+if (isNaN(età_utente)){
+    alert('devi inserire un numero per l\'età del passeggero')
+}
 
 // calcolo prezzo biglietto
 const tariffa = 0.21;
 
-let prezzo = (lunghezza_tratta * tariffa).toFixed(2);
+let prezzo 
 
 // calcolo sconto
 if (età_utente <= 18) {
@@ -19,5 +25,12 @@ if (età_utente <= 18) {
     prezzo = (((lunghezza_tratta * tariffa) / 5 ) * 3).toFixed(2);
     console.log(`Il biglietto costa :${prezzo}€ (tariffa anziani)`);
 } else {
+    prezzo = (lunghezza_tratta * tariffa).toFixed(2);
     console.log(`Il biglietto costa :${prezzo}€ (tariffa normale)`);
+}
+
+if (isNaN(prezzo)){
+    document.getElementById('prezzo_biglietto').innerHTML = `hai sbagliato ad inserire i dati`
+} else {
+    document.getElementById('prezzo_biglietto').innerHTML = `Il suo biglietto costa ${prezzo}€`
 }

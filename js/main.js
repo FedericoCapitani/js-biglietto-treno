da0ebd5e99940412bc23eaac1c0a84c20cbb2ced
FedericoCapitani <federico.capitani@outlook.it>
2022-03-25 09:50:14 +0100

    correggere errore messaggio del primo prompt

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto è definito in base ai km (0.21 € al km), va applicato uno sconto del 20% per i minorenni, va applicato uno sconto del 40% per gli over 65.
 
 // chiedere all'utente quanti chilometri è lunga la sua tratta
-const lunghezza_tratta = parseInt(prompt('Inserisci quani Km devi percorrere'));
+const lunghezza_tratta = parseInt(prompt('Inserisci quanti Km devi percorrere'));
 
 // chiedere all'utente l'età
 const età_utente = parseInt(prompt('Inserisci la tua età'));
